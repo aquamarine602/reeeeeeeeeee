5f647b64600bf8755f270285d169a91c7262153c
chloe.spilker <chloe.spilker@CLC-LAB209-04.TCSD.tooelesd.org>
2018-09-04 13:25:51 -0600

    try again?

---
+++ src/try_num_2/My_second_try.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package try_num_2;
+
+/**
+ * @author chloe.spilker
+ *
+ */
+public class My_second_try {
+
+	/**
+	 * 
+	 */
+	public My_second_try() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("this is my second try");
+
+	}
+
+}
